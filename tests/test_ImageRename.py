b203d6933ae6458563ed27f71d1676bd0f7287cd
kondakoffer <kondakov.valentin@icloud.com>
2023-06-14 19:11:58 +0200

    Complete get_exif_data_tests

--- tests/test_ImageRename.py
+++ tests/test_ImageRename.py
@@ -1,8 +1,9 @@
 """Tests for the renamer class."""
 import pytest
+import warnings
 
 import os
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 from ImageRename.renamer import *
 
@@ -43,31 +44,41 @@ class TestGroup_MinimalDateTime:
             date_times = ['1900:02:29 00:00:01'] # 1900 is not a leap year
             minimal_date_time = renamer._get_minimal_datetime(date_times)
 
-class TestGroup_GetExifDateTime:
+
+class TestGroup_GetExifDateTime():
     """Tests for _get_exif_datetimes function."""
-
-    # @pytest.mark.parametrize(
-            # ("standard_img")
-            # [
-            #     ('test_files/_DSC2428.JPG')
-            #     # Date Time: 8. Feb 2023 at 12:05:33
-            # ]
-    # )
-
-    def test_get_exif_datetimes(self):
-        with Image.open('tests/test_files/_DSC2428.JPG') as img:    
-            date_times = renamer._get_exif_datetimes(img)
-            assert date_times == [(306, 'DateTime', '2023:02:08 12:05:33')]
-
-    def test_no_date_time_found(self):
-        pass
-
-    def test_file_format_not_supported(self):
-        pass
-
-    def test_none(self):
-        pass
-
-class TestGroupRenameImage:
-    """Tests for rename_image function"""
     
+    @pytest.mark.parametrize(
+        'test_file_path,expected_date_times', 
+        [(
+            "tests/test_files/standard.JPG",
+            [(306, 'DateTime', '2023:02:08 12:05:33')] 
+        )]
+    )
+
+    def test_get_exif_datetimes(self, test_file_path, expected_date_times):
+        with Image.open(test_file_path) as img:
+            # Select a file which has date times for all three EXIF_Tags
+            warnings.warn(UserWarning("Use a testfile which has date times for all checked EXIF Tags"))
+            date_times = renamer._get_exif_datetimes(img)
+            assert date_times == expected_date_times
+
+    # def test_no_date_time_found(self):
+    #     # There currently is no file without date_times
+    #     warnings.warn(UserWarning("Provide a testfile which has no date-times in any checked EXIF Tag"))
+    #     return
+    #     with Image.open("tests/test_files/no_exif_datetime.JPG") as img:
+    #         date_times = renamer._get_exif_datetimes(img)
+    #         assert date_times == []
+
+    # def test_file_format_not_supported(self):
+    #     warnings.warn(UserWarning("More a test for rename_image function"))
+    #     with pytest.raises(UnidentifiedImageError):
+    #         with Image.open('tests/test_files/wrong_file_format.TXT') as img:
+    #             date_times = renamer._get_exif_datetimes(img)
+
+    # def test_none(self):
+    #     pass
+
+class TestGroup_RenameImage:
+    """Tests for rename_image function"""
